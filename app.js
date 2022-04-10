f215d29291cbc9221999dfc7bab4a5d7e8c116a8
Rahend23 <69079464+Rahend23@users.noreply.github.com>
2022-04-10 11:59:33 +0700

    Update app.js

--- app.js
+++ app.js
@@ -51,11 +51,11 @@ type koneksi : single,pool and request
 // );
 app.use(
     connection(mysql,{
-        host: process.env.MYSQLHOST,
-        user: process.env.MYSQLUSER, // your mysql user
-        password : process.env.MYSQLPASSWORD, // your mysql password
-        port : process.env.MYSQLPORT, //port mysql
-        database:process.env.MYSQLDATABASE // your database name
+        host: "containers-us-west-31.railway.app",
+        user: "root", // your mysql user
+        password : "HWdhNvZPC2miQnoQcgAL", // your mysql password
+        port : 6249, //port mysql
+        database:"railway" // your database name
     },'pool') //or single
 );
 app.use('/', index);
